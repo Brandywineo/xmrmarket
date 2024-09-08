18425375c6e0a6e0783b42580b046a0514e4c288
Brandywineo <152892618+Brandywineo@users.noreply.github.com>
2024-09-08 00:45:06 -0900

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const authRoutes = require('./routes/auth');
 
 app.use(express.json());
+app.use('/api', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Welcome to XMR Market!');
