23454064a1b1c78fff03c32992235bb6d58eb954
Brandywineo <152892618+Brandywineo@users.noreply.github.com>
2024-09-08 00:29:34 -0900

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-// server.js
 const express = require('express');
 const app = express();
-const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.send('Welcome to XMR Market!');
 });
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
