de03f0dc7d9c75e62743a500f7d4825cf47dab28
Brandywineo <152892618+Brandywineo@users.noreply.github.com>
2024-09-08 01:46:41 -0900

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/auth');
 
-router.post('/register', register);
-router.post('/login', login);
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.post('/login', (req, res) => {
+  // Authentication logic will go here
+  res.send('Login successful!');
+});
+
+router.get('/register', (req, res) => {
+  res.render('register');
+});
+
+router.post('/register', (req, res) => {
+  // Registration logic will go here
+  res.send('Registration successful!');
+});
 
 module.exports = router;
